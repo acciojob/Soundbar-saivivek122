159a454c9474ff911c6438bb8c492568d8b5b195
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-19 21:05:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const sounds = ["sound1", "sound2", "sound3"];
+
+const buttonsDiv = document.getElementById("buttons");
+
+sounds.forEach(sound => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSounds();
+    const audio = document.getElementById(sound);
+    audio.play();
+  });
+
+  buttonsDiv.appendChild(btn);
+
+ const audio = document.createElement("audio");
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+  document.body.appendChild(audio);
+});	
+
+const stopBtn = document.createElement("button");
+stopBtn.classList.add("stop");
+stopBtn.innerText = "Stop";
+stopBtn.addEventListener("click", stopSounds);
+buttonsDiv.appendChild(stopBtn);
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
